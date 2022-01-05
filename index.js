ced851c348058b2976eb12b49a2c3997f4107a00
JadenFicklin <jadendficklin@gmail.com>
2022-01-05 16:28:00 -0700

    created a simplified copy for understanding

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 let message = document.querySelector("#message");
-
 function addMovie(event) {
   event.preventDefault();
   let inputField = document.querySelector("input");
@@ -16,16 +15,14 @@ function addMovie(event) {
 
   let ulSeclector = document.querySelector("ul");
   ulSeclector.appendChild(movie);
-  inputField.value = " ";
+  inputField.value = "";
 }
 
 document.querySelector("form").addEventListener("submit", addMovie);
-
 function deleteMovie(event) {
   event.target.parentNode.remove();
   message.textContent = "movie deleted!";
 }
-
 function crossOffMovie(event) {
   event.target.classList.toggle("checked");
   if (event.target.classList.contains("checked") === true) {
@@ -34,3 +31,30 @@ function crossOffMovie(event) {
     message.textContent = "movie added back!";
   }
 }
+
+// Jadens simplified version so I can understand
+
+// function addMovie(event) {
+//   event.preventDefault();
+//   let inputSelector = document.querySelector("input");
+//   let movieListAdder = document.createElement("li");
+//   movieListAdder.textContent = inputSelector.value;
+//   movieListAdder.addEventListener("click", crossOffMovie);
+
+//   let xButton = document.createElement("button");
+//   xButton.textContent = "x";
+//   xButton.addEventListener("click", deleteMovie);
+//   movieListAdder.appendChild(xButton);
+
+//   let selectMovieInputFather = document.querySelector("ul");
+//   selectMovieInputFather.appendChild(movieListAdder);
+// }
+// document.querySelector("form").addEventListener("submit", addMovie);
+
+// function deleteMovie(event) {
+//   event.target.parentNode.remove();
+// }
+
+// function crossOffMovie(event) {
+//   event.target.classList.toggle("checked");
+// }
